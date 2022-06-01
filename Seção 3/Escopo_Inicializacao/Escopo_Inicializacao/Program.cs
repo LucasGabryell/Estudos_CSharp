5f13ef428ad8eef38e3288803d4a8d5cebe783b6
LucasGabryell <lucas.gabryell@souunit.com.br>
2022-06-01 17:02:25 -0300

    "Lógica de Programação com C#"

---
+++ Seção 3/Escopo_Inicializacao/Escopo_Inicializacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+
+            if (preco < 100)
+            {
+                double desconto = preco * 0.1;
+            }
+            //Console.WriteLine(desconto); não pode pois desconto é uma variavel local
+
+        }
+    }
+}
