90d72fcea46f5fe72aed8ed2737c470e8bf3c34d
LucasGabryell <lucas.gabryell@souunit.com.br>
2022-06-01 17:02:25 -0300

    "Lógica de Programação com C#"

---
+++ Seção 3 - LP usando C#/Exercicios_While/Exercicios_While/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1
+            Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha inválida!");
+                Console.Write("Digite a senha novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido!\n");
+
+
+            //Exercicio 2
+
+            Console.WriteLine("1 - Alcool \n2 - Gasolina \n3 - Disel \n4 - Sair");
+            int menu = int.Parse(Console.ReadLine());
+
+            int cont_alcool = 0;
+            int cont_gasolina = 0;
+            int cont_disel = 0;
+            while (menu != 4)
+            {
+                if (menu == 1)
+                {
+                    cont_alcool++;
+                }
+                else if(menu == 2)
+                {
+                    cont_gasolina++;
+                }
+                else if(menu == 3)
+                {
+                    cont_disel++;
+                }
+                Console.WriteLine("1 - Alcool \n2 - Gasolina \n3 - Disel \n4 - Sair");
+                menu = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO" +
+                "\nAlcool: " + cont_alcool + "\nGasolina: " + cont_gasolina +
+                "\nDisel: " + cont_disel);
+        }
+    }
+}
---
+++ Seção 3/Exercicios_For/Exercicios_For/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1
+            Console.Write("Digite quantos valores serão passados: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            int count_in = 0;
+            int count_out = 0;
+            for(int i = 1; i <= valores; i++)
+            {
+                Console.Write("Digite um número: ");
+                int num = int.Parse(Console.ReadLine());
+                if (num < 10 || num > 20)
+                {
+                    count_out++;
+                }
+                else if (num > 10 && num < 20)
+                {
+                    count_in++;
+                }
+            }
+            Console.WriteLine($"{count_in} in \n{count_out} out");
+
+            //Exercicio 2
+            Console.Write("Digite quantos valores serão passados: ");
+            int valores1 = int.Parse(Console.ReadLine());
+
+            for(int i = 1;i <= valores1; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
